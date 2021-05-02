b698ae6936058e57060c57e271d98a8673781004
mstatkus <mstatkus@gmail.com>
2021-05-02 23:20:17 +0300

    Update irk-maps-histograms.py
    added heatmaps

--- irk-maps-histograms.py
+++ irk-maps-histograms.py
@@ -49,6 +49,16 @@ def plot_hist(elt,caption=None,log=True,bins=50):
     plt.ylabel('Probability Density')
     if caption:
         plt.title(caption)
+        
+
+def np_skew(elt_map):
+    median = np.ma.median(elt_map)
+    mean = np.ma.mean(elt_map)
+    std = np.ma.std(elt_map)
+    
+    skew = (mean - median)/ std
+    
+    return (mean,median,std,skew)
 #%% load hspy
 hmap_file = 'IRK-31.hspy'
 hmap = hs.load(hmap_file)
@@ -75,6 +85,8 @@ plot_hist(si_f,log=False)
 cutoff = 35
 
 si_f_masked = np.ma.masked_less(si_f,cutoff)
+
+mask = si_f_masked.mask
 #%% plot four subplots for export
 
 vmax = si_f_masked.max()
@@ -82,11 +94,12 @@ vmin = si_f_masked.min()
 
 fig, axes = plt.subplots(2,2,dpi=300)
 
-plt.suptitle('Original datafile: {0}. Plotted on: {1.year}-{1.month}-{1.day} {1.hour}:{1.minute}:{1.second}'.format(orig_filename,datetime.now()),
+plt.suptitle('Original datafile: {0} Plotted on: {1.year}-{1.month}-{1.day} {1.hour}:{1.minute}:{1.second}'.format(orig_filename,datetime.now()),
              size='x-small',
              x=0.01,y=0,ha='left')
 
-axes[0,0].imshow(si_f,extent=extent,vmax=vmax,vmin=vmin)
+img00 = axes[0,0].imshow(si_f,extent=extent,vmax=vmax,vmin=0)
+fig.colorbar(img00,ax=axes[0,0],label='Intensity, cps',shrink=0.8,location='left')
 axes[0,0].set_title('Si K\u03b1 Line Map')
 
 axes[0,0].get_xaxis().set_visible(False)
@@ -98,7 +111,9 @@ scalebar_white = ScaleBar(1, "mm", length_fraction=0.25,
                           font_properties = {'size':'x-small'})
 axes[0,0].add_artist(scalebar_white)
 
-axes[1,0].imshow(si_f_masked,extent=extent,vmax=vmax,vmin=vmin)
+img10 = axes[1,0].imshow(si_f_masked,extent=extent,vmax=vmax,vmin=0)
+fig.colorbar(img10,ax=axes[1,0],label='Intensity, cps',shrink = 0.8,location='left')
+
 axes[1,0].set_title('Cutoff intensity = {} cps'.format(cutoff))
 
 axes[1,0].get_xaxis().set_visible(False)
@@ -133,8 +148,51 @@ scalebar_white = ScaleBar(1, "mm", length_fraction=0.25,
                           font_properties = {'size':'x-small'})
 axes[1,1].add_artist(scalebar_white)
 
-fig.tight_layout(pad=0.4, w_pad=3, h_pad=3.0)
+fig.tight_layout(w_pad=2.0, h_pad=2.0)
+#%%
 
 
+#%%
+
+elt_name = 'Sr'
+elt_map = get_element_map(elt_name, intensity_maps).data
+elt_map_masked = np.ma.array(elt_map,mask=mask)
+
+(mean,median,std,skew) = np_skew(elt_map_masked)
+
+cmap = cm.coolwarm
+vmin = elt_map_masked.min()
+vmax = elt_map_masked.max()
+
+divnorm = colors.TwoSlopeNorm(vmin=vmin, vcenter=median, vmax=vmax)
+#%%
+fig, axes = plt.subplots(2,1,dpi=300,squeeze=False)
+plt.suptitle('Original datafile: {0} Plotted on: {1.year}-{1.month}-{1.day} {1.hour}:{1.minute}:{1.second}'.format(orig_filename,datetime.now()),
+             size='x-small',
+             x=0.01,y=0,ha='left')
+
+img00 = axes[0,0].imshow(elt_map_masked,norm=divnorm,cmap=cmap,extent=extent)
+fig.colorbar(img00,ax=axes[0,0],label='Intensity, cps',shrink=0.8,location='left')
+axes[0,0].set_title('Elt Line Map')
+
+axes[0,0].get_xaxis().set_visible(False)
+axes[0,0].get_yaxis().set_visible(False)
+
+scalebar_black = ScaleBar(1, "mm", length_fraction=0.25,
+                          frameon=False,color='black',
+                          location='lower left',
+                          font_properties = {'size':'x-small'})
+axes[0,0].add_artist(scalebar_black)
+
+axes[1,0].hist(elt_map_masked.flatten(),
+               bins=50,
+               density=True,
+               histtype='step')
+axes[1,0].set_xlabel('Line Intensity, cps')
+axes[1,0].set_ylabel('Probability Density')
+axes[1,0].set_title('Elt Intensity Histogram')
 
 
+axes[1,0].axvline(median,color='r',label='Median')
+axes[1,0].axvline(mean,color='g',label='Mean')
+axes[1,0].legend()
