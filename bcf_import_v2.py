ca6eed8992d7f2ea811d03d743d7674a53fb4eb0
mstatkus <mstatkus@gmail.com>
2021-07-28 16:12:31 +0300

    Update bcf_import_v2.py
    refactoring  - classes

--- bcf_import_v2.py
+++ bcf_import_v2.py
@@ -4,182 +4,206 @@ Created on Sun Jul 25 13:40:28 2021
 
 @author: Михаил
 """
-
+#%% init
 import hyperspy.api as hs
 import hyperspy.io_plugins.bruker as bru
 import xml.etree.ElementTree as ET
-from PIL import Image
+# from PIL import Image
 import codecs
 import re
 import numpy as np
+import struct
+import matplotlib.pyplot as plt
 
-def load_bruker_bcf_as_xml(import_file):
-    """
-    Loads Bruker BCF file as XML,
-    useful for mosaic extraction
-    
-    Returns root xml tree object
-    """
+import logging
+_logger = logging.getLogger(__name__)
 
+     
 
-    # for mosaic extraction
-    bcf=bru.BCF_reader(import_file)
+class BCF_data():
     
-    header_file = bcf.get_file('EDSDatabase/HeaderData')
-    
-    header_byte_str = header_file.get_as_BytesIO_string().getvalue()
-    hd_bt_str = bru.fix_dec_patterns.sub(b'\\1.\\2', header_byte_str)
-    
-    root = ET.fromstring(hd_bt_str)
-    root = root.find("./ClassInstance[@Type='TRTSpectrumDatabase']")
-    
-    return root
+    def load_BCF(self,bcf_file):
+        self.bcf_file = bcf_file
+        bcf=bru.BCF_reader(bcf_file)
+        header_file = bcf.get_file('EDSDatabase/HeaderData')
+        header_byte_str = header_file.get_as_BytesIO_string().getvalue()
+        hd_bt_str = bru.fix_dec_patterns.sub(b'\\1.\\2', header_byte_str)  
+        root = ET.fromstring(hd_bt_str)
+        root = root.find("./ClassInstance[@Type='TRTSpectrumDatabase']")
+        self.root = root
 
-def extract_mosaic_from_xml(root):
+    def extract_mosaic(self):
+        """
+        Extracts mosaic RGB and calibration data from xml tree
+        Returns MosaicImageArray instance
+        """
+        node = self.root.find(
+            ".//ClassInstance[@Type='TRTImageData'][@Name='Default']")
         
-    """
-    Extracts mosaic RGB and calibration data from xml tree
-    """
-    node = root.find(
-        ".//ClassInstance[@Type='TRTImageData'][@Name='Default']")
+        
+        width = int(node.find('./Width').text)  # in pixels
+        height = int(node.find('./Height').text)  # in pixels
+        # image datatype in bytes ('u1','u2','u4')
+        dtype = 'u' + node.find('./ItemSize').text
+        
+        # calibration data: x_cal [um in 1 px]
+        x_cal = float(node.find('./XCalibration').text)  
+        y_cal = float(node.find('./YCalibration').text)
     
-    
-    width = int(node.find('./Width').text)  # in pixels
-    height = int(node.find('./Height').text)  # in pixels
-    # image datatype in bytes ('u1','u2','u4')
-    dtype = 'u' + node.find('./ItemSize').text
-    
-    # calibration data: x_cal [um in 1 px]
-    x_cal = float(node.find('./XCalibration').text)  
-    y_cal = float(node.find('./YCalibration').text)
+        plane_count = 3 #assuming RGB image
+        
+        planes = []
+        for i in range(plane_count):
+            img = node.find("./Plane" + str(i))
+            raw = codecs.decode(
+                (img.find('./Data').text).encode('ascii'), 'base64')
+            array1 = np.frombuffer(raw, dtype=dtype)
+            if any(array1):
+                data = array1.reshape((height, width))
+                planes.append(data)
+                
+        rgb_image_np = np.stack(planes,axis=-1)
+        
+        mosaic = MosaicImageArray(rgb_image_np)
+        mosaic.set_calibration((x_cal,y_cal))
+        return mosaic
 
-    plane_count = 3 #assuming RGB image
+    def extract_crop_coords(self):
+        """
+        Extracts crop coordinates for small mosaic
+        """
+        xpath = ".//ClassInstance[@Name='OverviewImageDescription']"
+        node = self.root.find(xpath)
+        node_txt = node.find('Text')
+        txt_raw = node_txt.text
+        txt_enc = codecs.decode(txt_raw.encode('ascii'), 'base64')
+        txt = txt_enc.decode('ascii')
+        
+        map_rect_regex = re.compile(
+            "MapRectMM=([0-9.E]+),([0-9.E]+),([0-9.E]+),([0-9.E]+)")
+        coords_str = re.findall(map_rect_regex,txt)[0]
+        x1,y1,w,h = [float(c) for c in coords_str] #x1, y1. w, h in mm
+        coords_mm = [x1,y1,x1+w,y1+h] #x1,y1, x2,y2
+        return coords_mm
     
-    planes = []
-    for i in range(plane_count):
-        img = node.find("./Plane" + str(i))
-        raw = codecs.decode(
-            (img.find('./Data').text).encode('ascii'), 'base64')
-        array1 = np.frombuffer(raw, dtype=dtype)
-        if any(array1):
-            data = array1.reshape((height, width))
-            planes.append(data)
+    def extract_stage_origin(self):
+        """
+        Get TRTSEMStageData coordinates:
+            origin X,Y,Z in _stage_ coordinate system
+        """
+        xpath = ".//ClassInstance[@Type='TRTSEMStageData']"
+        node = self.root.find(xpath)
+        coords = []
+        for L in list('XYZ'):
+            c = float(node.find('./{}'.format(L)).text)
+            coords.append(c)
+        return coords
+
+
+class MosaicImageArray():
+    def __init__(self, rgb_array):
+        self.rgb_array = rgb_array #np array (Y,X,3) - RGB
+        self.size_px = self.rgb_array.shape[0:2] # Y,X
+        self.calibrated = False
+    
+    
+    def set_calibration(self,calibration):
+        x_cal, y_cal = calibration #tuple of (x_cal,y_cal)
+        
+        self.calibration = (y_cal, x_cal) 
+        size_mm = []
+        for x_px, cal in zip(self.size_px,self.calibration):
+            x_mm = x_px * cal / 1000
+            size_mm.append(x_mm)
+        self.size_mm = tuple(size_mm) # Y, X
+        self.calibrated = True
+        self.extent = (0,size_mm[1], # X - left, right
+                       size_mm[0],0) # Y - top, bottom
+        
+    def imshow(self, ax=None, plot_axes = True,
+               **kwargs):
+        if ax is None:
+            ax = plt.gca()
+        if self.calibrated:
+            ax.imshow(X = self.rgb_array,
+                      extent = self.extent,
+                      **kwargs)
+        else:
+            _logger.info('Plotting uncalibrated image')
+            ax.imshow(X = self.rgb_array, **kwargs)
+        if plot_axes == False:
+            ax.get_xaxis().set_visible(False)
+            ax.get_yaxis().set_visible(False)
             
-    rgb_image_np = np.stack(planes,axis=-1)
-    pil_image = Image.fromarray(rgb_image_np)
-    
-    calibration = [x_cal,y_cal]
-    size_px = [width, height]
-    
-    return pil_image, size_px, calibration
-
-def extract_map_description(root):
-    """
-    Extracts map txt description data (crop coordinates for mosaic)
-    """
-    xpath = ".//ClassInstance[@Name='OverviewImageDescription']"
-    node = root.find(xpath)
-    node_txt = node.find('Text')
-    txt_raw = node_txt.text
-    txt_enc = codecs.decode(txt_raw.encode('ascii'), 'base64')
-    txt = txt_enc.decode('ascii')
-    return txt
-
-def get_crop_mosaic_coords_mm(txt_map_descr):
-    map_rect_regex = re.compile(
-        "MapRectMM=([0-9.E]+),([0-9.E]+),([0-9.E]+),([0-9.E]+)")
-    coords_str = re.findall(map_rect_regex,txt_map_descr)[0]
-    x1,y1,w,h = [float(c) for c in coords_str] #x1, y1. w, h in mm
-    coords_mm = [x1,y1,x1+w,y1+h] #x1,y1, x2,y2
-    return coords_mm
-
-# =============================================================================
-# def mosaic_origin(txt_map_descr):
-#     """
-#     Get MapStartPosMM coordinates: 
-#         origin X,Y,Z in _mosaic_ coordinate system,
-#     
-# 
-#     Parameters
-#     ----------
-#     txt_map_descr : TYPE
-#         DESCRIPTION.
-# 
-#     Returns
-#     -------
-#     coords_mm : TYPE
-#         DESCRIPTION.
-# 
-#     """
-#     map_rect_regex = re.compile(
-#         "MapStartPosMM=([0-9.E]+),([0-9.E]+),([0-9.E]+)")
-#     coords_str = re.findall(map_rect_regex,txt_map_descr)[0]
-#     coords_mm = [float(c) for c in coords_str] #x,y,z in mm
-#     return coords_mm
-# =============================================================================
-
-def stage_origin(root):
-    """
-    Get TRTSEMStageData coordinates:
-        origin X,Y,Z in _stage_ coordinate system
-
-    Parameters
-    ----------
-    root : TYPE
-        DESCRIPTION.
-
-    Returns
-    -------
-    coords : TYPE
-        DESCRIPTION.
-
-    """
-    xpath = ".//ClassInstance[@Type='TRTSEMStageData']"
-    node = root.find(xpath)
-    coords = []
-    for L in list('XYZ'):
-        c = float(node.find('./{}'.format(L)).text)
-        coords.append(c)
-    return coords
+    def crop(self, crop_coords_mm):
         
+        # crop_coords_mm = (x1,y1,x2,y2)
+        # self.calibration = (y_cal,x_cal) #FIXME
+        
+        crop_coords_px = []
+        for i,x in enumerate(crop_coords_mm):
+            cal = self.calibration[i%2]
+            x_px = round(x / cal *1000)
+            crop_coords_px.append(x_px)
+            
+        full = self.rgb_array
+        slice_y = slice(crop_coords_px[0],crop_coords_px[2])
+        slice_x = slice(crop_coords_px[1],crop_coords_px[3])
+        cropped = full[slice_x,slice_y,:]
+        
+        crop_mosaic = MosaicImageArray(cropped)
+        crop_mosaic.set_calibration(self.calibration)
+        
+        return crop_mosaic 
+        
+
+def load_bcf_dump_xml(bcf_file,xml_file):
+    '''
+    Loads bcf file, dumps header contents to XML file
+    s'''
+    # hd_bt_str = _load_bruker_bcf_as_bytestream(bcf_file)
     
+    bcf=bru.BCF_reader(import_file)
+    header_file = bcf.get_file('EDSDatabase/HeaderData')
+    header_byte_str = header_file.get_as_BytesIO_string().getvalue()
+    hd_bt_str = bru.fix_dec_patterns.sub(b'\\1.\\2', header_byte_str)  
+
+    xml_file = open(xml_file,'wb')
+    xml_file.write(hd_bt_str)
+    xml_file.close()
+
+def extract_coords_from_spx_xml(xml_root):
+    '''
+    Extracts stage coords from SPX file (not BCF!)
+
+    '''
+    node = xml_root.find(
+        ".//ClassInstance[@Type='TRTUnknownHeader'][@Name='RTREM']")
+
+    txt_raw =node.find('./Data').text
+    txt_enc = codecs.decode(txt_raw.encode('cp1252'), 'base64') # or ascii?
+    t_list = txt_enc.split(b'\x00') #split by 'empty' bytes
+    chunk=[x for x in t_list if len(x)==24][0] #get a chunk of 3*8 bytes - three floats
+    coords = struct.unpack('<ddd', chunk) #<ddd - three double floats
     
-
-def mm2px(x_mm,cal):
-    x_px = round(x_mm / cal *1000)
-    return x_px
-
-def px2mm(x_px,cal):
-    x_mm = x_px * cal / 1000
-    return x_mm
-
-def coords_mm2px(coords,calibrations):
-    coords_mm = []
-    for i,c in enumerate(coords):
-        coords_mm.append(mm2px(coords[i],calibrations[i%2]))
-    return coords_mm
-
-def coords_px2mm(coords,calibrations,round_place = 3):
-    coords_px = []
-    for i,c in enumerate(coords):
-        coords_px.append(np.round(px2mm(coords[i],calibrations[i%2]),
-                                  round_place))
-    return coords_px
-
-  
-def extent_from_coords(coords):
-    x1,y1,x2,y2 = coords
-    extent = (x1,x2,y2,y1)
-    return extent
+    return coords
 
 def stage2mosaic_coords(stage_zero,
                         crop_coords,
                         stage_coords):
+    '''
+    Converts stage coordinates to mosaic coordinates\
+    stage_zero - mm coordinates of left-top cropped mosaic corner
+                    in stage coordinagtes
+    crop_coords - mm coordinates of left-top cropped mosaic corner
+                    in full mosaic coordinagtes
+    '''
     mosaic_coords = []
     for i in range(2):
         q = stage_zero[i]+crop_coords[i]-stage_coords[i]
         mosaic_coords.append(q)
     return mosaic_coords
+
 
     
     
@@ -189,27 +213,32 @@ if __name__ == "__main__":
     print ('Test run')
     import_file = 'fang-A.bcf'
     
-    root_xml = load_bruker_bcf_as_xml(import_file)
-    mosaic_full, size_px, calibration = extract_mosaic_from_xml(root_xml)
+    # hd_bt_str = _load_bruker_bcf_as_bytestream(import_file)
+    # root_xml = _bcf_bytestream2xml(hd_bt_str)
+    bcf = BCF_data()
+    bcf.load_BCF(import_file)
+    mf = bcf.extract_mosaic()
     
-    stage_origin_mm = stage_origin(root_xml)
+    # mf = MosaicImageArray(mosaic_full)
+    # mf.set_calibration(calibration)
+    fig, ax = plt.subplots()
+    mf.imshow(ax=ax,plot_axes=True)
     
-    txt_map_descr = extract_map_description(root_xml)
-    crop_coords_mm = get_crop_mosaic_coords_mm(txt_map_descr)
-    # mosaic_origin_mm = mosaic_origin(txt_map_descr)
+    crop_coords_mm = bcf.extract_crop_coords()
+    stage_origin_mm = bcf.extract_stage_origin()
     
-    full_coords_px =[0,0] + size_px
-    full_coords_mm = coords_px2mm(full_coords_px, calibration,
-                                  round_place = 2)
-    
-    
-    crop_coords_px = coords_mm2px(crop_coords_mm, calibration)
-    
-    import matplotlib.pyplot as plt
+    # mc = mf.crop(crop_coords_mm)
+    # fig, ax = plt.subplots()
+    # mc.imshow(ax=ax)
 
 
-    extent = extent_from_coords(full_coords_mm)
-    plt.imshow(mosaic_full,extent = extent)
+
+    
+
+
+
+
+
     x = [99.149, 93.108, 81.709, 74.138] #coords from spx files
     y = [61.348, 59.679, 56.652, 66.077]
     
@@ -217,7 +246,7 @@ if __name__ == "__main__":
         pm = stage2mosaic_coords(stage_origin_mm,
                                  crop_coords_mm,
                                  p)
-        plt.scatter(pm[0],pm[1],color='white')
+        ax.scatter(pm[0],pm[1],color='white')
   
 
     
